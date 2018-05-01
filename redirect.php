010db4b0a0e36f304abebec2c5b375c3b0425678
emiljano539 <emiownage@yahoo.com>
2018-05-01 14:41:23 +0200

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Beta</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="css/tabela.css">
+<style type="text/css">
+img {
+    float: left;
+	width: 150px;	
+	height: 150px;
+	background-color: #0069D9;
+}
+</style>
+</head>
+<body>
+<?php
+
+$useragent = $_SERVER['HTTP_USER_AGENT'];
+
+if (strpos($useragent, "andiapp") === false) {
+
+  header("Location: http://albiptv.epizy.com/error404/");
+
+} ?>
+<a href="http://furious-sports.site/shqiptv/index.php"><div class="card"><h3 class="card-header">Kanalet Mix</h3><div class="card-block"><img src="https://cdn0.iconfinder.com/data/icons/communication-18/512/tv-512.png"></img><h4 class="card-title">Kanale me platformë dixhitale satelitore dhe tokësore Shqiptare </h4><a href="./digitalb.html" class="btn btn-primary">Shiko kanalet</a></div></div></a>
+<a href="http://dita24.info/MyWEBAPPLications/MM67CFD23TP9KL76HJGY12AZWVB856/APP-ITALIATv/VER1/index.html"><div class="card"><h3 class="card-header">Kanalet Italiane</h3><div class="card-block"><img src="https://www.whufc.com/sites/default/files/images/flags/2016-06/it.png"></img><h4 class="card-title">Kanalet Italiane</h4><a href="./tring.html" class="btn btn-primary">Shiko kanalet</a></div></div></a>
+
+<a href="http://vprxy.com/tuttosport/.m3u"><div class="card">
+<h3 class="card-header">Sportet e Dites</h3>
+<div class="card-block">
+<img src="https://www.gmkfreelogos.com/logos/S/img/Sport-4.gif"></img>
+<h4 class="card-title">Permban ndeshjet e dites</h4>
+<a href="http://vprxy.com/tuttosport/.m3u" class="btn btn-primary">Lista1</a> 
+</div></div></a>
+
+<a href="http://pastebin.com/raw/t1mBJ2Yi"><div class="card">
+<h3 class="card-header">iShqipe</h3>
+<div class="card-block">
+<img src="http://i.cubeupload.com/ERGb3N.jpg"></img>
+<h4 class="card-title">iShqipe</h4>
+<a href="http://pastebin.com/raw/t1mBJ2Yi" class="btn btn-primary">iShqipe</a>  
+</div></div></a>
+</body>
+</html>                            
